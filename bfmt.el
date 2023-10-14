42719915b8ef15917a42b61cea1d56919cb98698
Akira Komamura <akira.komamura@gmail.com>
2023-10-14 16:52:21 +0900

    Add header

--- bfmt.el
+++ bfmt.el
@@ -1,5 +1,36 @@
 ;;; bfmt.el --- A formatter frontend that applies changes in batch -*- lexical-binding: t -*-
 
+;; Copyright (C) 2023 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "29.1"))
+;; Keywords: files processes tools
+;; URL: https://github.com/akirak/bfmt.el
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; FIXME
+
+;;; Code:
+
 (defconst bfmt-formatter-output-buffer "*bfmt output*")
 
 (defcustom bmt-formatter-function #'bfmt-run-formatter-command
