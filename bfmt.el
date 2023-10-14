49b1640782ea9282d958f8afa0f25682a2560880
Akira Komamura <akira.komamura@gmail.com>
2023-10-14 16:03:07 +0900

    feat!: Allow the root locator to be a file name

--- bfmt.el
+++ bfmt.el
@@ -6,16 +6,19 @@
   "Function used to format files in a project."
   :type 'function)
 
-(defcustom bfmt-root-function #'bfmt-find-treefmt-nix
-  "Function used to determine the root of the project.
+(defcustom bfmt-root-location nil
+  "Function or file name used to determine the root of the project.
 
-The function should take the file name or the directory of the
-buffer as the argument and returns a directory. The returned
-directory can be either abbreviated or unabbreviated but should
-be consistent across all files under the same root.
+If it is a function, it should take the file name or the
+directory of the buffer as the argument and returns a directory.
+The returned directory can be either abbreviated or unabbreviated
+but should be consistent across all files under the same root.
+
+Alternatively, it can be a file name which indicates the root of the project.
 
 If the value is nil, `bfmt-enqueue-this-file' and `bfmt-apply' do nothing."
   :type '(choice function
+                 (string :tag "File name")
                  (const nil)))
 
 (defcustom bfmt-check-git-diff t
@@ -29,12 +32,15 @@ If the value is nil, `bfmt-enqueue-this-file' and `bfmt-apply' do nothing."
 (defvar bfmt-per-root-queues
   (make-hash-table :test #'equal :size bfmt-initial-project-map-size))
 
+(defun bfmt--find-root (file)
+  (cl-typecase bfmt-root-location
+    (function (funcall bfmt-root-location file))
+    (string (locate-dominating-file file bfmt-root-location))))
+
 ;;;###autoload
 (defun bfmt-apply ()
   "Apply formatter to files under the root directory."
-  (when-let (root (and bfmt-root-function
-                       (functionp bfmt-root-function)
-                       (funcall bfmt-root-function default-directory)))
+  (when-let (root (bfmt--find-root default-directory))
     (when-let (queue (gethash root bfmt-per-root-queues))
       (let* ((default-directory root)
              (files (cl-remove-duplicates queue :test #'equal))
@@ -58,10 +64,9 @@ If the value is nil, `bfmt-enqueue-this-file' and `bfmt-apply' do nothing."
 ;;;###autoload
 (defun bfmt-enqueue-this-file ()
   "Enqueue the buffer file to the formatting queue."
-  (when (and bfmt-root-function
-             (functionp bfmt-root-function))
+  (when bfmt-root-location
     (when-let* ((file (buffer-file-name))
-                (root (funcall bfmt-root-function file)))
+                (root (bfmt--find-root default-directory)))
       (let ((queue (gethash root bfmt-per-root-queues)))
         (unless (and queue (equal (car queue) file))
           (puthash root
@@ -69,9 +74,6 @@ If the value is nil, `bfmt-enqueue-this-file' and `bfmt-apply' do nothing."
                    bfmt-per-root-queues))))))
 
 ;;;; Specific formatters
-
-(defun bfmt-find-treefmt-nix (file)
-  (locate-dominating-file file "treefmt.nix"))
 
 (defun bfmt-nix-fmt (files)
   "Run \"nix fmt\" on FILES."
